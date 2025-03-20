83e6a77ad8850d7a605233da3dd7852d4fcaa47e
AdamHruska <ahruska155@gmail.com>
2025-03-20 17:44:48 +0100

    online meeting hide

--- components/addActivityCalendar.vue
+++ components/addActivityCalendar.vue
@@ -367,7 +367,7 @@ watch(dohodnute, (newValue) => {
 				>
 			</div>
 
-			<div class="relative z-0 w-full mb-5 group">
+			<div class="relative z-0 w-full mb-5 group" v-if="!onlineMeeting">
 				<label
 					class="text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 					>Miesto stretnutia</label
